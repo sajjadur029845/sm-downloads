cc728099a244eee65e60ceb2aa85a7f4a13a5319
zilladotexe <zilladotexe@gmail.com>
2016-04-06 00:25:19 -0400

    Created file

---
+++ source/Subs-SMDownloads.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SM Downloads
+ *
+ * @package forum/sm-downloads
+ * @author Jason Clemons <jason@simplemachines.org>
+ * @copyright 2016 Jason Clemons
+ * @license MIT
+ *
+ * @version 0.1.0
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+?>
