ec603ca910f34027dbedd04e96be8770696705b5
zilladotexe <zilladotexe@gmail.com>
2016-04-07 01:52:01 -0400

    Create SMDownloads-Moderate.php

---
+++ source/SMDownloads-Moderate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * SM Downloads
+ *
+ * @package forum/sm-downloads
+ * @author Jason Clemons <jason@simplemachines.org>
+ * @copyright 2016 Jason Clemons
+ * @license MIT
+ *
+ * @version 0.1.0
+ */
+
+if (!defined('SMF'))
+    die('No direct access...');
+
+function SMDownloadsAdmin()
+{
+    
+}
+
+?>
