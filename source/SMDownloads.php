c2447fc8a42be1fed6742fa505615fa866fc9d9c
zilladotexe <zilladotexe@gmail.com>
2016-04-07 01:49:24 -0400

    Added new actions
    Separated moderate and admin into new files
    Changed formatting

--- source/SMDownloads.php
+++ source/SMDownloads.php
@@ -15,21 +15,28 @@ if (!defined('SMF'))
 
 function SMDownloads()
 {
+    global $sourcedir;
+
     $subActions = array(
-        'add' => 'SMDownloadsAdd',
-        'comment' => 'SMDownloadsComment',
-        'download' => 'SMDownloadsDownload',
-        'main' => 'SMDownloadsMain',
-        'moderate' => 'SMDownloadsModerate',
-        'rate' => 'SMDownloadsRate',
-        'remove' => 'SMDownloadsRemove',
-        'view' => 'SMDownloadsView',
+        'add'       => array('SMDownloads.php', 'SMDownloadsAdd'),
+        'admin'     => array('SMDownloads-Admin.php', 'SMDownloadsAdmin'),
+        'comment'   => array('SMDownloads.php', 'SMDownloadsComment'),
+        'download'  => array('SMDownloads.php', 'SMDownloadsDownload'),
+        'featured'  => array('SMDownloads.php', 'SMDownloadsFeatured'),
+        'main'      => array('SMDownloads.php', 'SMDownloadsMain'),
+        'moderate'  => array('SMDownloads-Moderate.php', 'SMDownloadsModerate'),
+        'rate'      => array('SMDownloads.php', 'SMDownloadsRate'),
+        'remove'    => array('SMDownloads.php', 'SMDownloadsRemove'),
+        'stats'     => array('SMDownloads.php', 'SMDownloadsStats'),
+        'view'      => array('SMDownloads.php', 'SMDownloadsView'),
     );
 
     if (isset($_REQUEST['sa']) && in_array($subActions, $_REQUEST['sa']))
-        $subActions[$_REQUEST['sa']]();
+        require_once($sourcedir . '/' .$subActions[$_REQUEST['sa'][0]])
+        $subActions[$_REQUEST['sa'][1]]();
     else
-        $subActions['main']();
+        require_once($sourcedir . '/' . $subActions['main'][0]);
+        $subActions['main'][1]();
 }
 
 ?>
